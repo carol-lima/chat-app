baa8cd41969d2e33f95a60ed25094272f14a74c5
Carol Lima <carollima.dev@gmail.com>
2020-02-19 13:11:39 -0300

    Node server - user name, message, and connect/disconnect message

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Qual é o seu nome?')
+appendMessage('Você entrou!')
+socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
-	console.log(data);
+	appendMessage(`${data.name}: ${data.message}`)
 })
+
+socket.on('user-connected', name => {
+	appendMessage(`${name} entrou`)
+})
+
+socket.on('user-disconnected', name => {
+	appendMessage(`${name} saiu`)
+})
+
+messageForm.addEventListener('submit', e => {
+	e.preventDefault()
+	const message = messageInput.value
+	socket.emit('send-chat-message', message)
+	messageInput.value = ''
+})
+
+function appendMessage(message) {
+	const messageElement = document.createElement('div')
+	messageElement.innerText = message
+	messageContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const io = require('socket.io')(3000)
 
-io.on('connect', socket => {
-    socket.emit('chat-message', 'Hello!')
+const users = {}
+
+io.on('connection', socket => {
+	socket.on('new-user', name => {
+		users[socket.id] = name
+		socket.broadcast.emit('user-connected', name)
+	})
+
+	socket.on('send-chat-message', message => {
+		// Send message to every single other person connect on the server
+		// Except for the person that sent the message
+		socket.broadcast.emit('chat-message', {message: message, name: users[socket.id] })
+	})
+
+	socket.on('disconnect', message => {
+		socket.broadcast.emit('user-disconnected', users[socket.id])
+		delete users[socket.id]
+	})
 })
