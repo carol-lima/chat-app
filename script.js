f57c2474903fdf731cec0837f7b0bea22f34a7a7
Carol Lima <carollima.dev@gmail.com>
2020-02-25 22:35:16 -0300

    Saved layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
+const messageForm = document.getElementById('form-container')
 const messageInput = document.getElementById('message-input')
 
 const name = prompt('Qual é o seu nome?')
